bac67aca31d433d253c6b0608da03c4a6fc36eb6
tinkalguli <tinkalmunu@gmail.com>
2021-03-29 00:07:30 +0530

    Added migration to make title not nullable

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,3 @@
 class Poll < ApplicationRecord
-  
+  validates :title, presence: true, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_182329) do
+ActiveRecord::Schema.define(version: 2021_03_28_183432) do
 
   create_table "polls", force: :cascade do |t|
-    t.text "title"
+    t.text "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
