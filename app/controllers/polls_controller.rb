0d1880a4134c51006cce7249696c5a851f28be4f
tinkalguli <tinkalmunu@gmail.com>
2021-03-29 00:01:25 +0530

    Added polls controller and index action

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,5 @@
+class PollsController < ApplicationController
+  def index
+    render html: "This is index action of Polls controller"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, only: :index
+  resources :polls, only: :index
 
   root "home#index"
   get '*path', to: 'home#index', via: :all
